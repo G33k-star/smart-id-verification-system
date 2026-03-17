b4f2bfcdfc2807897f55939a27daecde3372d8f8
Jason S. <J23kirby@gmail.com>
2026-03-17 02:15:20 -0400

    Delete screens directory

--- screens/base_screen.py
+++
@@ -1,6 +0,0 @@
-import tkinter as tk
-
-class BaseScreen(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent, bg="white")
-        self.controller = controller
