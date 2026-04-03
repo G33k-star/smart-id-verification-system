e6332b983a9a475902c79073afceb36bede5f466
Jason S. <J23kirby@gmail.com>
2026-04-03 04:34:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from app import CheckInApp
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = CheckInApp(root)
-    root.mainloop()
+root = tk.Tk()
+app = CheckInApp(root)
+root.mainloop()
