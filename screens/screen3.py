16301a82742121d3bdb541de3752d20ac6185621
Jason S. <J23kirby@gmail.com>
2026-04-03 04:37:41 -0400

    Update screen3.py

--- screens/screen3.py
+++ screens/screen3.py
@@ -1,73 +1,65 @@
 import tkinter as tk
-from .base_screen import BaseScreen
 
-class Screen3(BaseScreen):
+
+class Screen3(tk.Frame):
     def __init__(self, parent, controller):
-        BaseScreen.__init__(self, parent, controller)
+        super().__init__(parent, bg="white")
 
-        top_bar = tk.Frame(self, bg="white")
-        top_bar.pack(fill="x", padx=20, pady=20)
+        self.controller = controller
 
         tk.Label(
-            top_bar,
+            self,
             text="Admin Login",
-            font=("Arial", 22, "bold"),
+            font=("Arial", 22),
+            fg="black",
             bg="white"
-        ).pack(side="left")
-
-        tk.Button(
-            top_bar,
-            text="Back",
-            width=12,
-            command=lambda: controller.show_frame("Screen1")
-        ).pack(side="right")
-
-        center = tk.Frame(self, bg="white")
-        center.pack(expand=True)
-
-        tk.Label(center, text="Username", font=("Arial", 12), bg="white").pack(pady=(10, 5))
-        self.username_entry = tk.Entry(
-            center,
-            textvariable=controller.admin_user_var,
-            width=35,
-            font=("Arial", 12)
-        )
-        self.username_entry.pack(pady=(0, 12))
-
-        tk.Label(center, text="Password", font=("Arial", 12), bg="white").pack(pady=(5, 5))
-        self.password_entry = tk.Entry(
-            center,
-            textvariable=controller.admin_pass_var,
-            show="*",
-            width=35,
-            font=("Arial", 12)
-        )
-        self.password_entry.pack(pady=(0, 15))
-
-        tk.Button(
-            center,
-            text="Check Admin",
-            width=18,
-            command=controller.check_admin_credentials
-        ).pack(pady=10)
+        ).pack(pady=20)
 
         self.message_label = tk.Label(
-            center,
+            self,
             text="",
             font=("Arial", 12),
-            bg="white",
-            fg="red"
+            fg="red",
+            bg="white"
         )
-        self.message_label.pack(pady=10)
+        self.message_label.pack(pady=5)
 
-    def set_message(self, message):
-        self.message_label.config(text=message)
+        form_frame = tk.Frame(self, bg="white")
+        form_frame.pack(pady=20)
 
-    def focus_username(self):
-        self.username_entry.focus_set()
+        tk.Label(form_frame, text="Username:", fg="black", bg="white").grid(row=0, column=0, sticky="e", pady=5, padx=5)
+        self.username_entry = tk.Entry(form_frame, textvariable=controller.admin_user_var, width=30)
+        self.username_entry.grid(row=0, column=1, pady=5, padx=5)
+
+        tk.Label(form_frame, text="Password:", fg="black", bg="white").grid(row=1, column=0, sticky="e", pady=5, padx=5)
+        self.password_entry = tk.Entry(form_frame, textvariable=controller.admin_pass_var, width=30, show="*")
+        self.password_entry.grid(row=1, column=1, pady=5, padx=5)
+
+        button_frame = tk.Frame(self, bg="white")
+        button_frame.pack(pady=20)
+
+        tk.Button(
+            button_frame,
+            text="Login",
+            width=15,
+            command=controller.check_admin_credentials
+        ).grid(row=0, column=0, padx=10)
+
+        tk.Button(
+            button_frame,
+            text="Back",
+            width=15,
+            command=lambda: controller.show_frame("Screen1")
+        ).grid(row=0, column=1, padx=10)
 
     def reset_screen(self):
         self.controller.admin_user_var.set("")
         self.controller.admin_pass_var.set("")
         self.set_message("")
         self.focus_username()
+
+    def set_message(self, text):
+        self.message_label.config(text=text)
+
+    def focus_username(self):
+        self.username_entry.focus_set()
