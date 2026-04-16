399d2f1fc363460e323a0be79641f21be3accb76
Erick Licona <92254077+YaBraG@users.noreply.github.com>
2026-04-16 15:52:41 -0400

    Change to fullscreen

--- main.py
+++ main.py
@@ -2,5 +2,13 @@ import tkinter as tk
 from app import CheckInApp
 
 root = tk.Tk()
+root.attributes("-fullscreen", True)
+root.attributes("-topmost", True)
+root.configure(cursor="none")
+root.bind("<Escape>", lambda e: "break")
+root.bind("<Alt-Key>", lambda e: "break")
+root.bind("<Control-Key>", lambda e: "break")
+root.resizable(False, False)
+
 app = CheckInApp(root)
 root.mainloop()
